335a4633fdca388ce0bb44c9794245561a19eb55
Wellinton Rafael Gudilunas <111888592+WellintonGudilunas@users.noreply.github.com>
2023-02-12 21:06:24 -0300

    .

---
+++ aula63Splice/aula63.js
@@ -0,0 +1,23 @@
+const nomes = ['Kaike', 'Cleito', 'Leomidas', 'Gepeto', 'Aluizio'];
+
+// const removidos = nomes.splice(-4, Number.MAX_VALUE);
+//nomes.splice(INDICE A SER REMOVIDO, QUANTOS ELEMENTOS INCLUINDO ELE, A PARTIR DESSE INDICE DEVEM SER REMOVIDOS?)
+//nomes.splice(indice, delete, elementos para adicionar separado por ,);
+
+
+//POP()
+// const removidos = nomes.splice(-1, 1);
+
+//SHIFT()
+// const removidos = nomes.splice(0, 1);
+
+//UNSHIFT()
+// nomes.splice(0, 0, 'Teste', 'Testante');
+
+//PUSH()
+// nomes.splice(nomes.length, 0, 'Teste', 'Testante');
+
+
+
+
+console.log(nomes);
