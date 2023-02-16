8f70ff2a1fe5ce43042fb7c2ae4cdca158277f39
Wellinton Rafael Gudilunas <111888592+WellintonGudilunas@users.noreply.github.com>
2023-02-15 23:53:25 -0300

    .

---
+++ aula46SetIntervalSetTimeOut/aula46.js
@@ -0,0 +1,20 @@
+const mostraHora = () => {
+    let data = new Date();
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    });
+}
+
+const timer = setInterval(function (){
+    console.log(mostraHora());
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(timer);
+}, 3000);
+
+
+setTimeout(function(){
+    console.log('Tst')
+}, 5000)
