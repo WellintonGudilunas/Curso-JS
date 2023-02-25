aecb359871b2951132e496f0936d4ead4d87401b
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-02-24 21:41:07 -0300

    Commit aula 10 Views

---
+++ node/aula10Views/server.js
@@ -0,0 +1,22 @@
+// npm i
+// npm i ejs !!! IMPORTANT
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({extended: true}));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+
+
+app.use(routes);
+
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor porta 3000');
+}); 
+
