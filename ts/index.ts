5114947a42c0ffa9fce13f830f031ad17e72365e
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-03-20 01:02:04 -0300

    Configurando Eslint e Prettier

---
+++ ts/index.ts
@@ -0,0 +1,5 @@
+//npm i @typescript-eslint/eslint-plugin @typescript-eslint/parser -D;
+//npm i prettier eslint-config-prettier eslint-plugin-prettier -D;
+const nome = 'teste';
+const sobrenome = 'teste';
+console.log(nome, sobrenome);
