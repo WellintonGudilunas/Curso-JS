25ccd28c129f524e21b06c98401c0988cc5dab6b
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-03-21 00:15:13 -0300

    Configurando tsconfig.json

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,5 +1,5 @@
 //npm i @typescript-eslint/eslint-plugin @typescript-eslint/parser -D;
 //npm i prettier eslint-config-prettier eslint-plugin-prettier -D;
-const nome = 'teste';
-const sobrenome = 'teste';
-console.log(nome, sobrenome);
+import funcao from './mod';
+
+funcao();
